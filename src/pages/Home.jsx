07af4d2984c52af2d7b07511a0baae54e15dc85c
oluwakorede <alobakehinde05@gmail.com>
2024-05-20 13:20:55 +0100

    vercel commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
 // eslint-disable-next-line no-unused-vars
-import mobile from "../assets/home/background-home-mobile.jpg"
-import './home.css' 
- 
+import './home.css'
+
 const Home = () => {
   return (
-    <div  className="bg w-screen md:bg-cover md:bg-home-tablet  lg:bg-home-desktop bg-cover text-white  bg-no-repeat z-0  h-dvh flex flex-col gap-20  ">
-      <style>
-        background-image: url({mobile})
-      </style>
+    <div className="bg  w-screen md:bg-cover  bg-cover text-white  bg-no-repeat z-0  h-dvh flex flex-col gap-20  ">
       {/* <img className='w-full z-[-100]  h-dvh' src={bgHomeMbImg} alt="" /> */}
       <div className="px-5 gap-10 flex lg:items-start xl:px-44 xl:mt-[300px] lg:px-80 lg:mt-[600px]  md:mt-40 flex-col items-center mt-16 ">
         <p className="barlow-condensed-regular text-[#D0D6F9] md:text-[25px]">
