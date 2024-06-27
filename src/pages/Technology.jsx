6c15197fedbf48a59936ef2aa3f320744ee29681
oluwakorede <alobakehinde05@gmail.com>
2024-06-27 07:41:41 +0100

    technology

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -20,17 +20,17 @@ const Technology = () => {
     //Two styles are applied 
     //1. the tailwind styling is applied (bg-technology-mobile md:bg-technology-tablet xl:bg-technology-desktop).
     //2. .css styling because vercel does not recognize the tailwind styling.
-    <div className="bge  w-screen md:pt-[10px] md:mb-10 md:mt-[-10px]  bg-cover text-white  bg-no-repeat z-0 h-screen">
+    <div className="bge md:h-[1190px]   md:pt-[0px]  md:mt-[-10px] xl:h-[1000px]  md:mb-0 xl:mb-0 md:overflow-hidden  xl:overflow-hidden bg-cover text-white  bg-no-repeat z-0 h-screen">
       <Navbar/>
-      <div className="gap-10 flex text-left md:items-start md:text-[20px] md:tracking-wider tracking-wider text-[16px] md:mt-40 md:gap-16  w-screen flex-col items-center ">
+      <div className="gap-10 flex text-left  md:items-start md:text-[20px] md:tracking-wider tracking-wider text-[16px] md:mt-40 md:gap-16  w-screen flex-col items-center ">
         <p className="barlow-condensed-regular xl:text-[28px] xl:pl-[200px] pt-3 md:pl-12 flex h-[19px] text-[16px] gap-3  ">
           <p className="text-[#383B4B]">03</p> SPACE LAUNCH 101
         </p>
         <img src={technology[index].images.landscape} alt="" className="w-full xl:hidden " />
         <img src={technology[index].images.portrait} alt="" className="w-[515px] h-[527px] absolute right-0 top-[180px] xl:flex hidden " />
       </div>
-      <div className={`pt-8 `}>
-        <div className="barlow-condensed-thin text-center xl:absolute   xl:pl-1 xl:left-[200px] xl:top-[300px] xl:flex-col md:pl-80 flex gap-6 items-center pl-[125px] ">
+      <div className={`pt-8  `}>
+        <div className="barlow-condensed-thin text-center xl:absolute    xl:pl-1 xl:left-[200px] xl:top-[300px] xl:flex-col md:pl-80 flex gap-6 items-center pl-[125px] ">
           <p
             className={
               index === 0
@@ -69,14 +69,14 @@ const Technology = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center xl:text-start  xl:absolute xl:left-[320px]  xl:top-[260px] gap-[11px] pt-10  leading-7">
+      <div className="flex flex-col items-center  xl:text-start  xl:absolute xl:left-[320px]  xl:top-[260px] gap-[11px] pt-10  leading-7">
         <h2 className="barlow-condensed-regular xl:text-start xl:text-[16px]  xl:mr-[300px] tracking-wide leading-4 md:text-[16px]  text-[14px] text-[#D0D6F9] h-[17px] w-[132px] text-center">
           THE TERMINOLOGY...
         </h2>
         <h1 className="bellefair-regular xl:text-start md:text-[40px] xl:pl-[] xl:text-[56px]  xl:w-[480px]  xl:ml-[45px] md:w-[400px] md:pt-[16px] text-[24px] uppercase pb-4 w-[327px] text-center h-[28px]">
           {technology[index].name}
         </h1>
-        <p className="barlow-condensed-regular xl:leading-6   xl:text-[16px] md:leading-6 xl:pr-[38px]  xl:mr-[80px] xl:text-start flex md:text-[16px] md:w-[350px] md:pt-[20px] text-center left-[24px] text-[#D0D6F9]  leading-7 text-[15px] font-normal w-[327px] h-[100px] top-[125px] ">
+        <p className="barlow-condensed-regular xl:leading-6    xl:text-[16px] md:leading-6 xl:pr-[38px]  xl:mr-[80px] xl:text-start flex md:text-[16px] md:w-[350px] md:pt-[20px] text-center left-[24px] text-[#D0D6F9]  leading-7 text-[15px] font-normal w-[327px] h-[100px] top-[125px] ">
           {technology[index].description}
         </p>
       </div>
